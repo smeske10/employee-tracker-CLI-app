d2e904933fdfe623c155303aad17062824207086
smeske10 <35307525+smeske10@users.noreply.github.com>
2022-02-26 18:04:16 -0500

    Added CRUD Operations to index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function inquire() {
         message: 'Select an action',
         name: 'select',
         choices: [
-          "View all Departments",
+          "View all department",
           "View all Roles",
           "View all Employees",
           "View Employees by Manager",
@@ -37,8 +37,8 @@ function inquire() {
         ]
       }]).then(function (res) {
         switch (res.select) {
-          case "View all Departments":
-            return viewDepartments();
+          case "View all department":
+            return viewdepartment();
 
           case "View all Roles":
             return viewRoles();
@@ -46,38 +46,38 @@ function inquire() {
           case "View all Employees":
             return viewEmployees();
 
-          // case "View Employees by Manager":
-          //   viewEmployeesByManager();
+          case "View Employees by Manager":
+           return viewEmployeesByManager();
 
-          // case "View Employees by Department":
-          //   viewEmployeesByDepartment();
+          case "View Employees by Department":
+            return viewEmployeesByDepartment();
 
-          // case "Add Department":
-          //   addDepartment();
+          case "Add Department":
+            return addDepartment();
 
-          // case "Add Role":
-          //   addRole();
+          case "Add Role":
+            return addRole();
 
-          // case "Add Employee":
-          //   addEmployee();
+          case "Add Employee":
+            return addEmployee();
 
-          // case "Update Employee Role":
-          //   updateEmployeeRole();
+          case "Update Employee Role":
+            return updateEmployeeRole();
 
-          // case "Update Employee Manager":
-          //   updateEmployeeManager();
+          case "Update Employee Manager":
+            return updateEmployeeManager();
 
-          // case "Delete Department":
-          //   deleteDepartment();
+          case "Delete Department":
+            return deleteDepartment();
 
-          // case "Delete Role":
-          //   deleteRole();
+          case "Delete Role":
+            return deleteRole();
 
-          // case "Delete Employee":
-          //   deleteEmployee();
+          case "Delete Employee":
+            return deleteEmployee();
 
-          // case "View Total Utilized Budget of Department":
-          //   viewBudget();
+          case "View Total Utilized Budget of Department":
+            return viewBudget();
         }
       })
 };
@@ -106,7 +106,7 @@ function anotherQuery() {
     })
 }
 
-function viewDepartments() {
+function viewdepartment() {
   const showDepartment = () => {
     sql.query("SELECT * FROM department;", (err, results) => { console.log(consoleTable.getTable(results)) });
     setTimeout(() => {
@@ -130,7 +130,7 @@ function viewRoles() {
 
 function viewEmployees() {
   const showEmployees = () => {
-    sql.query("SELECT id, first_name, last_name, role_id, manager_id FROM employee;",
+    sql.query("SELECT * FROM employee;",
      (err, results) => { console.log(consoleTable.getTable(results)) });
     setTimeout(() => {
       anotherQuery();
@@ -139,3 +139,439 @@ function viewEmployees() {
   showEmployees()
 }
 
+function viewEmployeesByManager() {
+  const showEmployeesbyManager = () => {
+    sql.query("SELECT * FROM employee LEFT JOIN roles ON employee.role_id = role.id LEFT JOIN department ON ;",
+     (err, results) => { console.log(consoleTable.getTable(results)) });
+    setTimeout(() => {
+      anotherQuery();
+    }, 100);
+  }
+  showEmployeesbyManager()
+}
+
+
+function viewEmployeesByDepartment() {
+  const showEmployeesbyDepartment = () => {
+    sql.query("SELECT employees.id,first_name,last_name,title,department,salary,employees.manager_id FROM employees INNER JOIN roles ON employees.role_id = roles.id INNER JOIN department ON roles.department_id = department.id;", (err, results) => { console.log(consoleTable.getTable(results)) });
+    setTimeout(() => {
+      anotherQuery();
+    }, 100);
+  }
+  showEmployeesbyDepartment()
+}
+
+function addDepartment() {
+  const newDepartment = () => {
+    return inquirer.prompt([
+      {
+        type: "input",
+        name: "departmentName",
+        message: "Please enter a new Department name to add:"
+      }
+    ])
+  }
+  newDepartment()
+    .then((data) => {
+      sql.query(`INSERT INTO department(name) VALUES("${data.departmentName}")`, (err, results) => { console.log("Department Added") })
+    })
+    .then((data) => {
+      sql.query(`SELECT *FROM department`, (err, results) => { console.log(consoleTable.getTable(results)) })
+    })
+    .then((data) => {
+      setTimeout(() => {
+        anotherQuery();
+      }, 100);
+    })
+}
+
+function addRole() {
+  const newRole = () => {
+    let addedRoles = []
+    sql.query("select * from department", (err, results) => { addedRole(results) })
+    function addedRole(value) {
+      for (let i = 0; i < value.length; i++) {
+        addedRoles.push({
+          name: value[i]["department"],
+          value: value[i]["id"]
+        })
+      }
+    }
+    return inquirer.prompt([
+      {
+        type: "input",
+        name: "roleName",
+        message: "Please enter a new role to add:"
+      },
+      {
+        type: "input",
+        name: "salary",
+        message: "Please enter this role's salary:"
+      },
+      {
+        type: "list",
+        name: "deptChoice",
+        message: "Please select of the following:",
+        choices: addedRoles
+      }
+    ])
+  }
+  newRole()
+    .then((data) => {
+      sql.query(`INSERT INTO roles(title, salary, department_id) VALUES("${(data.roleName)}","${data.salary}","${data.deptChoice}")`, (err, results) => {
+        if (err) { throw err }
+      })
+    })
+    .then((data) => {
+      sql.query(`SELECT *FROM roles`, (err, results) => { console.log(consoleTable.getTable(results)) })
+    })
+    .then((data) => {
+      setTimeout(() => {
+        anotherQuery();
+      }, 100);
+    })
+}
+
+function addEmployee() {
+  let addedEmployees = []
+  sql.query("select * from employees", (err, results) => {
+    if (err) { console.log(err) }
+    addedEmployee(results)
+  })
+  function addedEmployee(value) {
+    for (let l = 0; l < value.length; l++) {
+      addedEmployees.push({
+        name: value[l]["first_name"],
+        value: value[l]["id"]
+      })
+    }
+  }
+
+  let rtd = []
+  sql.query("select * from roles", (err, results) => { addRTd(results) })
+  function addRTd(value) {
+    for (let i = 0; i < value.length; i++) {
+      rtd.push({
+        name: value[i]["title"],
+        value: value[i]["id"]
+      })
+    }
+  }
+  const newEmployee = () => {
+    return inquirer.prompt([
+      {
+        type: "input",
+        name: "firstName",
+        message: "Please enter the new employee's first name."
+      },
+      {
+        type: "input",
+        name: "lastName",
+        message: "Please enter the new employee's last name."
+      },
+      {
+        type: "list",
+        name: "deptChoice",
+        message: "Please select a roll:",
+        choices: rtd
+      },
+      {
+        type: "list",
+        name: "manager",
+        message: "Please select a manager:",
+        choices: addedEmployees
+      }
+    ])
+  }
+  newEmployee()
+    .then((data) => {
+      sql.query(`INSERT INTO employees(first_name, last_name, role_id, manager_id) VALUES("${data.firstName}","${data.lastName}","${data.deptChoice}","${data.manager}")`,
+        (err, results) => { console.log("Employee Added") })
+    })
+    .then((data) => {
+      sql.query(`SELECT employees.id,first_name,last_name,title,department,salary,employees.manager_id FROM employees INNER JOIN roles ON employees.role_id = roles.id INNER JOIN department ON roles.department_id = department.id;`,
+        (err, results) => { console.log(consoleTable.getTable(results)) })
+    })
+    .then((data) => {
+      setTimeout(() => {
+        anotherQuery();
+      }, 100);
+    })
+}
+
+function updateEmployeeRole() {
+  let addedEmployees2 = []
+  sql.query("SELECT * FROM employees", (err, results) => { updateaddedEmployees(results) })
+  function updateaddedEmployees(value) {
+    for (let l = 0; l < value.length; l++) {
+      addedEmployees2.push({
+        name: value[l]["first_name"],
+        value: value[l]["id"]
+      })
+    }
+  }
+
+  let rtd2 = []
+  sql.query("SELECT * FROM roles", (err, results) => { updateRTd(results) })
+  function updateRTd(value) {
+    for (let n = 0; n < value.length; n++) {
+      rtd2.push({
+        name: value[n]["title"],
+        value: value[n]["id"]
+      })
+    }
+  }
+  const newEmployeeUpdate = () => {
+    return inquirer.prompt([
+      {
+        type: "input",
+        name: "test",
+        message: "Changes are permanent, press enter to continue",
+      },
+      {
+        type: "list",
+        name: "chosenEmployee",
+        message: "Please select an employee to change their role:",
+        choices: addedEmployees2
+      },
+      {
+        type: "list",
+        name: "updatedRole",
+        message: "Please select a new role:",
+        choices: rtd2
+      }
+    ])
+  }
+  newEmployeeUpdate()
+    .then((data) => {
+      sql.query(`UPDATE employees SET role_id = ${data.updatedRole} WHERE id =${data.chosenEmployee}`,
+        (err, results) => { console.log("Employee Updated") })
+    })
+    .then((data) => {
+      sql.query(`SELECT employees.id,first_name,last_name,title,department,salary,employees.manager_id FROM employees INNER JOIN roles ON employees.role_id = roles.id INNER JOIN department ON roles.department_id = department.id;`,
+        (err, results) => { console.log(consoleTable.getTable(results)) })
+    })
+    .then((data) => {
+      setTimeout(() => {
+        anotherQuery();
+      }, 100);
+    })
+}
+
+function updateEmployeeManager() {
+  let addedEmployees2 = []
+  sql.query("SELECT * FROM employees", (err, results) => { updateaddedEmployees(results) })
+  function updateaddedEmployees(value) {
+    for (let l = 0; l < value.length; l++) {
+      addedEmployees2.push({
+        name: value[l]["first_name"],
+        value: value[l]["id"]
+      })
+    }
+  }
+
+  let rtd2 = []
+  sql.query("SELECT * FROM roles", (err, results) => { updateRTd(results) })
+  function updateRTd(value) {
+    for (let n = 0; n < value.length; n++) {
+      rtd2.push({
+        name: value[n]["title"],
+        value: value[n]["id"]
+      })
+    }
+  }
+  const newEmployeeUpdate = () => {
+    return inquirer.prompt([
+      {
+        type: "input",
+        name: "test",
+        message: "Changes are permanent, press enter to continue",
+      },
+      {
+        type: "list",
+        name: "chosenEmployee",
+        message: "Please select an employee to change their role:",
+        choices: addedEmployees2
+      },
+      {
+        type: "list",
+        name: "updatedRole",
+        message: "Please select a new role:",
+        choices: rtd2
+      }
+    ])
+  }
+  newEmployeeUpdate()
+    .then((data) => {
+      sql.query(`UPDATE employees SET role_id = ${data.updatedRole} WHERE id =${data.chosenEmployee}`,
+        (err, results) => { console.log("Employee Updated") })
+    })
+    .then((data) => {
+      sql.query(`SELECT employees.id,first_name,last_name,title,department,salary,employees.manager_id FROM employees INNER JOIN roles ON employees.role_id = roles.id INNER JOIN department ON roles.department_id = department.id;`,
+        (err, results) => { console.log(consoleTable.getTable(results)) })
+    })
+    .then((data) => {
+      setTimeout(() => {
+        anotherQuery();
+      }, 100);
+    })
+}
+
+function deleteDepartment() {
+  const deletedDepartment = () => {
+    return inquirer.prompt([
+      {
+        type: "input",
+        name: "deptName",
+        message: "Please enter a new Department name to add:"
+      }
+    ])
+  }
+  deletedDepartment()
+    .then((data) => {
+      sql.query(`INSERT INTO department(department) VALUES("${data.deptName}")`, (err, results) => { console.log("Department Added") })
+    })
+    .then((data) => {
+      sql.query(`SELECT *FROM department`, (err, results) => { console.log(consoleTable.getTable(results)) })
+    })
+    .then((data) => {
+      setTimeout(() => {
+        anotherQuery();
+      }, 100);
+    })
+}
+
+function deleteRole() {
+  const deletedRole = () => {
+    let deletedRolesList = []
+    sql.query("select * from department", (err, results) => { deletedRoles(results) })
+    function deletedRoles(value) {
+      for (let i = 0; i < value.length; i++) {
+        deletedRolesList.push({
+          name: value[i]["department"],
+          value: value[i]["id"]
+        })
+      }
+    }
+    return inquirer.prompt([
+      {
+        type: "input",
+        name: "roleName",
+        message: "Please enter a new role to add:"
+      },
+      {
+        type: "input",
+        name: "salary",
+        message: "Please enter this role's salary:"
+      },
+      {
+        type: "list",
+        name: "deptChoice",
+        message: "Please select of the following:",
+        choices: deletedRolesList
+      }
+    ])
+  }
+  deletedRole()
+    .then((data) => {
+      sql.query(`INSERT INTO roles(title, salary, department_id) VALUES("${(data.roleName)}","${data.salary}","${data.deptChoice}")`, (err, results) => {
+        if (err) { throw err }
+      })
+    })
+    .then((data) => {
+      sql.query(`SELECT *FROM roles`, (err, results) => { console.log(consoleTable.getTable(results)) })
+    })
+    .then((data) => {
+      setTimeout(() => {
+        anotherQuery();
+      }, 100);
+    })
+}
+
+function deleteEmployee() {
+  let addedEmployees = []
+  sql.query("select * from employees", (err, results) => {
+    if (err) { console.log(err) }
+    addedEmployee(results)
+  })
+  function addedEmployee(value) {
+    for (let l = 0; l < value.length; l++) {
+      addedEmployees.push({
+        name: value[l]["first_name"],
+        value: value[l]["id"]
+      })
+    }
+  }
+
+  let rtd = []
+  sql.query("select * from roles", (err, results) => { addRTd(results) })
+  function addRTd(value) {
+    for (let i = 0; i < value.length; i++) {
+      rtd.push({
+        name: value[i]["title"],
+        value: value[i]["id"]
+      })
+    }
+  }
+  const deletedEmployee = () => {
+    return inquirer.prompt([
+      {
+        type: "input",
+        name: "firstName",
+        message: "Please enter the new employee's first name."
+      },
+      {
+        type: "input",
+        name: "lastName",
+        message: "Please enter the new employee's last name."
+      },
+      {
+        type: "list",
+        name: "deptChoice",
+        message: "Please select a roll:",
+        choices: rtd
+      },
+      {
+        type: "list",
+        name: "manager",
+        message: "Please select a manager:",
+        choices: addedEmployees
+      }
+    ])
+  }
+  deletedEmployee()
+    .then((data) => {
+      sql.query(`INSERT INTO employees(first_name, last_name, role_id, manager_id) VALUES("${data.firstName}","${data.lastName}","${data.deptChoice}","${data.managerChoice}")`,
+        (err, results) => { console.log("Employee Added") })
+    })
+    .then((data) => {
+      sql.query(`SELECT employees.id,first_name,last_name,title,department,salary,employees.manager_id FROM employees INNER JOIN roles ON employees.role_id = roles.id INNER JOIN department ON roles.department_id = department.id;`,
+        (err, results) => { console.log(consoleTable.getTable(results)) })
+    })
+    .then((data) => {
+      setTimeout(() => {
+        anotherQuery();
+      }, 100);
+    })
+}
+
+function viewBudget() {
+  const budget = () => {
+    let deletedRolesList = []
+    sql.query("select * from department", (err, results) => { deletedRoles(results) })
+    function deletedRoles(value) {
+      for (let i = 0; i < value.length; i++) {
+        deletedRolesList.push({
+          name: value[i]["department"],
+          value: value[i]["id"]
+        })
+      }
+    }
+  }
+  budget()
+  .then((data) => {
+    setTimeout(() => {
+      anotherQuery();
+    }, 100);
+  });
+}
